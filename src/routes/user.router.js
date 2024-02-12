6907bf759c67c297455d5dbe09ac8fc5a1c86054
Luis Cerchiaro <5613256+Luis-Cerchiaro@users.noreply.github.com>
2024-02-12 14:06:13 -0500

    reset password

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,27 +1,47 @@
-
-
-const { getAll, create, getOne, remove, update, verifyUser, login, logged } = require('../controllers/user.controllers');
-const express = require('express');
-const { verifyJwt } = require('../utils/verifyJWT');
+const {
+  getAll,
+  create,
+  getOne,
+  remove,
+  update,
+  verifyUser,
+  login,
+  logged,
+  resetPassword,
+  updatePassword
+} = require("../controllers/user.controllers");
+const express = require("express");
+const { verifyJwt } = require("../utils/verifyJWT");
 
 const routerUser = express.Router();
 
-routerUser.route('/')
-    .get(verifyJwt, getAll)
-    .post(create);
+//Static routes
 
-routerUser.route('/verify/:code')
-.get(verifyUser)
+routerUser.route("/")
+  .get(verifyJwt, getAll)
+  .post(create);
 
-routerUser.route('/login')
-  .post(login)
+routerUser.route("/login")
+  .post(login);
 
-  routerUser.route('/me')
-  .get(verifyJwt, logged)
+routerUser.route("/me")
+  .get(verifyJwt, logged);
 
-routerUser.route('/:id')
-    .get(verifyJwt, getOne)
-    .delete(verifyJwt, remove)
-    .put(verifyJwt, update);
+routerUser.route("/reset_password")
+  .post(resetPassword);
+
+//Dynamic routes
+
+routerUser.route("/reset_password/:code")
+  .post(updatePassword);
+
+routerUser.route("/verify/:code")
+  .get(verifyUser);
+
+routerUser
+  .route("/:id")
+  .get(verifyJwt, getOne)
+  .delete(verifyJwt, remove)
+  .put(verifyJwt, update);
 
 module.exports = routerUser;
